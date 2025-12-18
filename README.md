b11827fdb6256be9189e35a0b2380fa12eb6bb9e
Martin Rixham <martin.rixham@gmail.com>
2025-12-18 09:41:37 +0000

    Sum over totals with same SKU.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # SuppChain
 basic calc functions for supply chain data
+
+## Run
+To run the script execute `./total.py orders.csv`
--- total.py
+++ total.py
@@ -1,15 +1,25 @@
-import pandas as pandas
+#! /usr/bin/env python3
 
-def total(orders):
+import sys
+import pandas
+
+def year_total(orders):
     return pandas.DataFrame({
         "SKU": orders["SKU"],
         "SUM": orders.iloc[:, 1:].sum(axis=1)
     })
 
-def main():
-    orders = pandas.read_csv("orders.csv")
+def sku_total(totals):
+    return totals.groupby("SKU")["SUM"].sum().reset_index()
 
-    print(total(orders))
+def main():
+    if len(sys.argv) >= 2:
+        file_name = sys.argv[1]
+        orders = pandas.read_csv(file_name)
+
+        print(sku_total(year_total(orders))) 
+    else:
+        print("Error: No data file provided.", file=sys.stderr)
 
 if __name__ == '__main__':
     main()
