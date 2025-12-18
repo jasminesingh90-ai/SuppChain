fb4fb5886f6f95601b5a363bf9f118a9437846a4
Martin Rixham <martin.rixham@gmail.com>
2025-12-18 17:57:53 +0000

    Error handling for file reading.

--- total.py
+++ total.py
@@ -12,14 +12,30 @@ def year_total(orders):
 def sku_total(totals):
     return totals.groupby("SKU")["SUM"].sum().reset_index()
 
+def print_errors(messages):
+    for message in messages:
+        print("Error: " + message, file=sys.stderr)
+
 def main():
+    file_name = None
+    orders = None
+    errors = [];
+
     if len(sys.argv) >= 2:
         file_name = sys.argv[1]
-        orders = pandas.read_csv(file_name)
+    else:
+        errors.append("No data file provided.")
 
+    if file_name is not None:
+        try:
+            orders = pandas.read_csv(file_name)
+        except FileNotFoundError:
+            errors.append("File %s does not exist." % file_name)
+    
+    if len(errors) == 0:
         print(sku_total(year_total(orders))) 
     else:
-        print("Error: No data file provided.", file=sys.stderr)
+        print_errors(errors)
 
 if __name__ == '__main__':
     main()
